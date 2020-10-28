1f0fca4907ed5b067823c9bbf986317639c563b0
Eudes Guzmán <eudesgg19@gmail.com>
2020-10-28 16:58:58 +0000

    first commit

--- src_copy/models.py
+++ src_copy/models.py
@@ -2,10 +2,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    firstName = db.Column(db.String(80), unique=False, nullable=False)
+    lastName = db.Column(db.String(80), unique=False, nullable=False)
+
+    #relation
+    followers = db.relationship('Follower', lazy=True)
+
+    comments = db.relationship('Comment', lazy=True)
+
+    posts = db.relationship('Post', lazy=True)
+    
+
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "username" : self.username,
+            "firstName": self.firstName,
+            "lastName": self.lastName
+            # do not serialize the password, its a security breach
+        }
 
 class Follower(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    userName = db.Column(db.String(50), unique=False, nullable=False)
+    userName = db.Column(db.String(80), unique=False, nullable=False)
 
     #relation
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -18,58 +46,14 @@ class Follower(db.Model):
         return {
             "id": self.id,
             "username" : self.username
-        }
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userName = db.Column(db.String(50), unique=False, nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    firstName = db.Column(db.String(80), unique=False, nullable=False)
-    lastName = db.Column(db.String(80), unique=False, nullable=False)
-
-    #relation
-    followers = db.relationship('Follower', lazy=True)
-    comments = db.relationship('Comment', lazy=True)
-    posts = db.relationship('Post', lazy=True)
-    
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            "username" : self.username,
-            "firstName": self.firstName,
-            "lastName": self.lastName
-        }
-
-class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    commentText = db.Column(db.Text, unique=False, nullable=False)
-
-    #relation many to one - user
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship("User")
-
-    #relation many to one - post
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
-    post = db.relationship("Post")
-
-    def __repr__(self):
-        return '<Comment %r>' % self.commentText
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "commentText" : self.commentText
+            # do not serialize the password, its a security breach
         }
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(100))
     date = db.Column(db.String(20))
+    
 
     #relation many to one user
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -86,12 +70,37 @@ class Post(db.Model):
             "id": self.id,
             "content" : self.content,
             "date" : self.date
+            # do not serialize the password, its a security breach
         }
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    commentText = db.Column(db.Text, unique=False, nullable=False)
+
+    #relation many to one user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User")
+
+    #relation many to one post
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    post = db.relationship("Post")
+
+    def __repr__(self):
+        return '<Comment %r>' % self.commentText
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "commentText" : self.commentText
+            # do not serialize the password, its a security breach
+        }
+
 
 class Media(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tipo = db.Column(db.Integer)
-    url = db.Column(db.String(80))
+    url = db.Column(db.String(20))
 
     #relation many to one post
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
@@ -105,4 +114,5 @@ class Media(db.Model):
             "id": self.id,
             "tipo" : self.tipo,
             "url" : self.url
+            # do not serialize the password, its a security breach
         }
